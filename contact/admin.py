1476a20c181efa11535726930575b7e83876bdb0
Luan Rocha <luanvr2001@gmail.com>
2023-12-04 09:16:26 -0300

    finalizado models e inserido dados fake no banco de dados

--- contact/admin.py
+++ contact/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 
-from contact.models import Contact
+from contact.models import Contact, Category
 
 @admin.register(Contact)
 class AdminContact(admin.ModelAdmin):
-    ...
+    list_display = 'id', 'first_name', 'last_name', 'phone',
+    ordering = 'id',
+    search_fields = 'id', 'first_name',
+    list_per_page = 5
+
+
+@admin.register(Category)
+class AdminCategory(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = 'id',
--- contact/models.py
+++ contact/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
 
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
@@ -8,6 +15,18 @@ class Contact(models.Model):
     email = models.EmailField(blank=True)
     created_date = models.DateField(default=timezone.now)
     description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/m')
+    category = models.ForeignKey(
+        Category, 
+        on_delete=models.SET_NULL,
+        blank=True, null=True
+        )
+    owner = models.ForeignKey(
+        User, 
+        on_delete=models.SET_NULL,
+        blank=True, null=True
+        )
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
