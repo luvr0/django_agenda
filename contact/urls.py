84dbd3c42d4a4d649308d147efabc9055d60a82f
Luan Rocha <luanvr2001@gmail.com>
2023-12-06 21:59:36 -0300

    criado a query e alguns ajustes no código

--- contact/urls.py
+++ contact/urls.py
@@ -5,5 +5,6 @@ app_name = 'contact'
 
 urlpatterns = [
     path('<int:id>/', views.contact, name='contact'),
+    path('search/', views.search, name='search'),
     path('', views.home, name='index'),
 ]
--- contact/views/views.py
+++ contact/views/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
 from contact.models import Contact
 
 
@@ -7,6 +8,31 @@ def home(request):
 
     text = {
         'contacts': contacts,
+        'title_web': 'Contatos -',
+    }
+
+    return render(
+        request,
+        'contact/index.html',
+        text
+    )
+
+def search(request):
+    search_module = request.GET.get('q', '').strip()
+
+    if search_module == '':
+        return redirect('contact:index')
+
+    contacts = Contact.objects.filter(show=True).filter(
+        Q(first_name__icontains=search_module) |
+        Q(last_name__icontains=search_module)  |
+        Q(email__icontains=search_module) |
+        Q(phone__icontains=search_module)
+        )
+
+    text = {
+        'contacts': contacts,
+        'title_web': 'Pesquisa -',
     }
 
     return render(
@@ -18,8 +44,11 @@ def home(request):
 def contact(request, id):
     single_contact = get_object_or_404(Contact.objects.filter(pk=id), show=True)
 
+    title_web = f'{single_contact.first_name} {single_contact.last_name} -'
+
     text = {
         'contact': single_contact,
+        'title_web': title_web,
     }
 
     return render(
